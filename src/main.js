1f0866fe775506036d20a89277f84b22f6c8dae5
songke <1154285845@qq.com>
2018-08-07 20:41:19 +0800

    配置路由对象

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
+import router from './routes/router';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/css/index.css';
 new Vue({
+  router,
   el: '#app',
   render: h => h(App)
 })
---
+++ src/routes/router.js
@@ -0,0 +1,11 @@
+import VueRouter from 'vue-router';
+import Vue from 'vue';
+Vue.use(VueRouter);
+//创建路由  路由规则
+var router = new VueRouter({
+  routes: [
+
+  ]
+});
+// 模块中导出路由对象
+export default router;
