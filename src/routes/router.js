69de84dd03d7a420faa6d8f9fe37743e7490d26b
songke <1154285845@qq.com>
2018-08-07 21:58:33 +0800

    路由规则

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,17 @@
 import VueRouter from 'vue-router';
 import Vue from 'vue';
+// 导入组件
+import HeroesList from '../views/heroes/List.vue';
+import EquipsList from '../views/equips/List.vue';
+import WeaponsList from '../views/weapons/List.vue';
+// 注册VueRouter插件
 Vue.use(VueRouter);
 //创建路由  路由规则
 var router = new VueRouter({
   routes: [
-
+    { name: 'heroes', path: '/heroes', component: HeroesList },
+    { name: 'equips', path: '/equips', component: EquipsList },
+    { name: 'weapons', path: '/weapons', component: WeaponsList }
   ]
 });
 // 模块中导出路由对象
