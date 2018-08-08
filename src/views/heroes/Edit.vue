ff2c844012738e8db6017ca679990bec0536cf2a
songke <1154285845@qq.com>
2018-08-08 21:44:56 +0800

    编辑页面显示英雄信息

--- src/views/heroes/Edit.vue
+++ src/views/heroes/Edit.vue
@@ -10,35 +10,54 @@
         <label for="sex">英雄性别</label>
         <input v-model="formData.gender" type="text" class="form-control" id="sex" placeholder="英雄性别">
       </div>
-      <button @click.prevent="handleAdd" type="submit" class="btn btn-success">Submit</button>
+      <button @click.prevent="handleEdit" type="submit" class="btn btn-success">Submit</button>
     </form>
   </div>
 </template>
  <script>
-export default {
-  data() {
-    return {
-      formData: {
-        name: '',
-        gender: ''
-      }
-    }
-  }
-};
-</script>
- <style>
- </style>
-<script>
-  export default {
-    data() {
-      return {
-        formData: {
-          name: '',
-          gender: ''
+    import axios from 'axios';
+    export default {
+      props: ['id'],
+      data() {
+        return {
+          formData: {
+            name: '',
+            gender: ''
+          }
         }
-      }
-    }
-  }
+      },
+       created () {
+          axios
+            .get(`http://127.0.0.1:3001/heroes/${this.id}`)
+            .then((response)=>{
+              if(response.status == 200){
+                console.log(response)
+                this.formData = response.data;
+              }
+            })
+            .catch((err)=>{
+              alert('修改失败');
+            })
+        },
+        methods : {
+            handleEdit () {
+              axios
+                .put(`http://127.0.0.1:3001/heroes/${this.id}`,this.formData)
+                 .then((response)=> {
+                    if(response.status === 200 ) {
+                       this.$router.push('/heroes')
+                    }else {
+                      alert('修改失败')
+                    }
+                 })
+                 .catch((err) => {
+                   console.log(err)
+                 })
+
+            }
+
+        }
+    };
 </script>
 <style>
 
