13700724a28d7cb4feac86ebad477e4686c046e8
songke <1154285845@qq.com>
2018-08-08 19:00:52 +0800

    渲染英雄列表

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-      <!-- herder -->
-      <app-header></app-header>
-      <div class="container-fluid">
-        <div class="row">
-          <!-- sider -->
-          <app-sidebar></app-sidebar>
-          <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          </div>
+    <!-- 网页的header -->
+    <app-header></app-header>
+    <div class="container-fluid">
+      <div class="row">
+        <!-- sidebar -->
+        <app-sidebar></app-sidebar>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <!-- 列表管理 -->
           <router-view></router-view>
         </div>
       </div>
+    </div>
   </div>
 </template>
 <script>
